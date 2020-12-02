9fae098a4ffc05a0b5f96522ccca40fe7a06407d
James Ross <james.i.ross5@gmail.com>
2020-12-02 00:51:17 +0000

    Add advent day 1 - finding numbers summing to 2020

---
+++ src/day01.py
@@ -0,0 +1,50 @@
+
+from typing import Tuple
+
+Vector = Tuple[int]
+
+def puzzle_01(input_file: str) -> Vector:
+    """
+    Takes a data file containing a list of integers (one per line),
+    finds the two integers summing 2020 and multiplies them together
+
+    :param input_file: Name of file containing data
+
+    :returns: Tuple - (first number, second number, two numbers multiplied)
+    """
+    with open(f"inputs/{input_file}") as f:
+        numbers = sorted([int(line.strip()) for line in f.readlines()])
+
+    for number in numbers:
+        if 2020 - number in numbers:
+            return number, 2020 - number, number * (2020 - number)
+
+
+def puzzle_02(input_file: str) -> Vector:
+    """
+    Takes a data file containing a list of integers (one per line),
+    finds the two integers summing 2020 and multiplies them together
+
+    :param input_file: Name of file containing data
+
+    :returns: Tuple - (first number, second number, two numbers multiplied)
+    """
+    with open(f"inputs/{input_file}") as f:
+        numbers = sorted([int(line.strip()) for line in f.readlines()])
+
+    for index, number1 in enumerate(numbers):
+        difference = 2020 - number1
+        for number2 in numbers[index:]:
+
+            if difference - number2 in numbers:
+                return (
+                    number1,
+                    number2,
+                    difference - number2,
+                    number1 * number2 * (difference - number2)
+                )
+
+
+if __name__ == "__main__":
+
+    print(puzzle_02("day_1"))
